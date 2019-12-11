6c9c2b428055eecdd005e4709d1cd36f4ec852a4
Zongming Lin <zongminglin@outlook.com>
2019-12-11 16:17:18 +0800

    Init Project

---
+++ Tango.Core/Identity/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tango.Core.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ Tango.Infrastructure/Identity/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tango.Core.Identity;
+
+namespace Tango.Infrastructure.Identity
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
